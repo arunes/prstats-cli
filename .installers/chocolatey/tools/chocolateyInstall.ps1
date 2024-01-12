ee62902b66934ad0e28d141ab51d3c05668b970f
arunes <ali@arunes.com>
2024-01-12 01:00:40 -0600

    feat: adding env variable

--- .installers/chocolatey/tools/chocolateyInstall.ps1
+++ .installers/chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName= 'prstats-cli'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url        = 'tools/prstats-cli-win-x86-v0.1.0.0.msi'
-$url64      = 'tools/prstats-cli-win-x64-v0.1.0.0.msi'
+$url        = 'https://github.com/arunes/prstats-cli/releases/download/v0.1/prstats-cli-win-x86-v0.1.0.0.msi'
+$url64      = 'https://github.com/arunes/prstats-cli/releases/download/v0.1/prstats-cli-win-x64-v0.1.0.0.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -11,9 +11,9 @@ $packageArgs = @{
   silentArgs    = "/qn /norestart"
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'prstats-cli*'
-  checksum      = '5AA26246FDF559F6CE0133578CC2424556E0B054B32CE14017540524AF30DA3C'
+  checksum      = '9A70F2998DCE08AA24613827F3395F08DE6928803584A06C9D209546CAA69397'
   checksumType  = 'sha256'
-  checksum64    = '0BD9BEBFFBC9A61C78EF91EE051C5E5AB9A364772E95F64D73DA7E04668278D4'
+  checksum64    = '98CF6586B17FBCAD9A1367CC7ED3F4F72E93C185D77CAC3D38B2E4B454DFA002'
   checksumType64= 'sha256'
 }
 
